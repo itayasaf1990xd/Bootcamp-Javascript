ff2cdf5014f5847817775bcd88ddfabe85c7c99a
itayasaf1990xd <89840492+itayasaf1990xd@users.noreply.github.com>
2021-11-28 16:39:44 +0200

    Add files via upload

--- Exercise 69 - Create Web Page/scripts/index.js
+++ Exercise 69 - Create Web Page/scripts/index.js
@@ -1,21 +1,27 @@
-const container = document.createElement('div');
+const container = document.createElement("div");
 document.body.appendChild(container);
 
-const title = document.createElement('h1');
-const img = document.createElement('img');
+const title = document.createElement("h1");
+const img = document.createElement("img");
+const subtitle = document.createElement("h3");
 
 container.appendChild(title);
 container.appendChild(img);
+container.appendChild(subtitle);
 
 Object.assign(container.style, {
-    display: 'flex',
-    alignItems: 'center',
-    flexFlow: 'column'
-})
-
-title.textContent = 'Johnny Bryce';
-
-img.src = 'john-bryce.jpg'
-Object.assign(img.style, {
-    width: '200px'
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexFlow: "column",
+  height: "100vh",
 });
+
+title.textContent = "Johnny Bryce";
+
+img.src = "./assets/images/john-bryce.jpg";
+Object.assign(img.style, {
+  width: "400px",
+});
+
+subtitle.textContent = "The best training company in the world since 1990";
